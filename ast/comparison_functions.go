509bdc572f78d2e93e638e2e83acc006bc762489
Jacob Simpson <jacob.simpson@gmail.com>
2021-03-28 22:31:47 -0700

    Lift variable resolution to the top of le.

--- ast/comparison_functions.go
+++ ast/comparison_functions.go
@@ -44,34 +44,31 @@ func lt(environment map[string]string, left, right Value) bool {
 }
 
 func le(environment map[string]string, left, right Value) bool {
+	left = resolveVar(environment, left.Value())
+
 	switch l := left.(type) {
-	case *VarValue:
+	case *AnyValue:
 		switch r := right.(type) {
 		case *DateTimeValue:
-			return dateTimeGTAny(r, resolveVar(environment, left.Value())) ||
-				dateTimeEQAny(r, resolveVar(environment, left.Value()))
+			return dateTimeGTAny(r, l) || dateTimeEQAny(r, l)
 		case *IntegerValue:
-			return integerGTAny(r, resolveVar(environment, left.Value())) ||
-				integerEQAny(r, resolveVar(environment, left.Value()))
+			return integerGTAny(r, l) || integerEQAny(r, l)
 		case *DoubleValue:
-			return doubleGTAny(r, resolveVar(environment, left.Value())) ||
-				doubleEQAny(r, resolveVar(environment, left.Value()))
+			return doubleGTAny(r, l) || doubleEQAny(r, l)
 		default:
 			return false
 		}
 	case *DateTimeValue:
-		switch right.(type) {
-		case *VarValue:
-			return dateTimeLTAny(l, resolveVar(environment, right.Value())) ||
-				dateTimeEQAny(l, resolveVar(environment, right.Value()))
+		switch r := right.(type) {
+		case *AnyValue:
+			return dateTimeLTAny(l, r) || dateTimeEQAny(l, r)
 		default:
 			return false
 		}
 	case *IntegerValue:
-		switch right.(type) {
-		case *VarValue:
-			return integerLTAny(l, resolveVar(environment, right.Value())) ||
-				integerEQAny(l, resolveVar(environment, right.Value()))
+		switch r := right.(type) {
+		case *AnyValue:
+			return integerLTAny(l, r) || integerEQAny(l, r)
 		default:
 			return false
 		}
